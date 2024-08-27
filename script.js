0f22bc11c940f013d1bc7e9d3a9aed5761bcbde5
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-08-27 15:58:49 +0200

    layout and css

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('JS OK');
+
+// Prendo elementi dal DOM
+// Metto il timer da 30 secondi
+// Genero 5 numeri casuali
+// Sostituisco i numeri casuali con gli input
+// Stampo i numeri che l'utente ha indovinato
+
+
+const timer = document.getElementById('timer');
+const numbersElement = document.getElementById('mem-numbers');
+const inputSection = document.getElementById('div-input');
+const playButton = document.querySelector('button');
+const messageElement = document.getElementById('message');
